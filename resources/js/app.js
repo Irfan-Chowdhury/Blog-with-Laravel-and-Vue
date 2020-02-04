5b0fedf6416fa3bf14bfa6dec1907285ff72ffe3
IRFAN <irfanchowdhury80@gmail.com>
2020-02-05 00:09:18 +0600

    How to use Vue (basic)

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,17 +9,25 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter) 
 
 
-
-
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-
+const routes = [  //16,22 routes name should be same
+    // { path: '/foo', component: Foo },
+    // { path: '/bar', component: Bar }
+  ]
 
 const router = new VueRouter({
-    routes // short for `routes: routes`
+    routes, // short for `routes: routes`
+    mode:'history' //by using this '#' is not show in url 
   })
 
 const app = new Vue({
     el: '#app',
     router //can access all router after put this here  
 });
+
+
+//1. command: npm run watch
+//2. app.js
+//3. ExampleComponent.vue
+//4. adminmaster.blade.php
